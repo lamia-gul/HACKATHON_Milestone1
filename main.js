40988cb691a36a239acef930c6c790a63e63eebf
lamia-gul <164479840+lamia-gul@users.noreply.github.com>
2024-09-08 12:41:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+// script.ts
+// Function to toggle the visibility of all fieldsets
+function toggleFieldsetsVisibility() {
+    // Select all fieldset elements
+    var fieldsets = document.querySelectorAll('fieldset');
+    // Iterate over each fieldset and toggle its visibility
+    fieldsets.forEach(function (fieldset) {
+        if (fieldset.style.display === 'none') {
+            fieldset.style.display = 'block';
+        }
+        else {
+            fieldset.style.display = 'none';
+        }
+    });
+}
+// Event listener for the toggle button
+var toggleButton = document.getElementById('toggleSectionsBtn');
+if (toggleButton) {
+    toggleButton.addEventListener('click', toggleFieldsetsVisibility);
+}
